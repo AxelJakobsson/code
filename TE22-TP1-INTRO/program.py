e619a58b2d35cfd1170f627e48d36cc33544caa3
TheHoboDad <axejak056@gmail.com>
2024-09-04 13:43:11 +0200

    Skrev hur blackjack skulle fungera

---
+++ TE22-TP1-INTRO/program.py
@@ -0,0 +1,18 @@
+#Ess kort kan räknas som 1 eller 11. 
+
+# Dra två kort, med värden 1 till 14. 
+# Kolla ifall kortvärdena är lika med 21, ifall det är det så slutar man spela och Spelaren får blackjack.
+# Ifall kortvärdena är över 21 så blir spelaren bust och dealern vinner. 
+
+# Annars drar dealern ett kort. 
+# Sen får spelaren valet att antingen stanna eller dra ett till kort. 
+# Ifall spelaren drar ett kort och kommer över 21 så blir spelaren bust och dealern vinner.
+
+# Ifall dealerns kort är under 17 så drar den kort, ifall dealern har ett äss och värdena kan bli 17 eller högre ifall man räknar esset som 11 så måste den stå (inte dra).
+# Om dealerns kort är över 17 så måste den stå.
+
+# Ifall spelarens kort är under 21 så får den stå eller dra.
+
+# Detta fortsätter tills alla spelare står
+
+# Den som är närmast 21 vinner, ifall spelare och dealer är lika så vinner dealern.
