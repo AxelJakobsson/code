993e6a5381cc5032925be75ceb39cfc001fd764e
TheHoboDad <axejak056@gmail.com>
2024-09-10 11:03:56 +0200

    commit!

--- TE22-TP1-INTRO/SevenSpel.py
+++ TE22-TP1-INTRO/SevenSpel.py
@@ -1,9 +1,13 @@
 import random
 
+money = 100
+betMoney = 1
+
+
 while True:
-    bet = input("Seven, Under or Over: ").lower()
-    if bet in ["seven", "under", "over"]:
-        break
+        bet = input("Seven, Under or Over: ").lower()
+        if bet in ["seven", "under", "over"]:
+            break
 
 
 dice1 = random.randint(1,6)
@@ -15,9 +19,14 @@ print(sum)
 
 if sum < 7 and bet == 'under':
     print("You won")
+    money + (betMoney*2)
+
 elif sum == 7 and bet == 'seven':
     print("You won")
+    money + (betMoney*3)
 elif sum > 7 and bet == 'over':
     print("You won")
+    money + (betMoney*2)
 else:
     print("You lost.")
+    money + (betMoney*0)
