a15a9d5d01564d8fb5aab590ac7dc7372521f268
TheHoboDad <axejak056@gmail.com>
2024-09-10 13:45:41 +0200

    commit

--- TE22-TP1-INTRO/SevenSpel.py
+++ TE22-TP1-INTRO/SevenSpel.py
@@ -1,32 +1,42 @@
 import random
 
 money = 100
-betMoney = 1
+betMoney = 0
+
+while money > 0:
+    while True:
+            while True:
+                 print(f"You have {money} money left.")
+                 betMoney = int(input("How much do you want to bet? "))
+                 if betMoney > 0 and betMoney <= money:
+                      break
+                 else:
+                      print(f"Invalid bet you can bet between 1 and {money}")
+                
+            bet = input("Seven, Under or Over: ").lower()
+            if bet in ["seven", "under", "over"]:
+                break
 
 
-while True:
-        bet = input("Seven, Under or Over: ").lower()
-        if bet in ["seven", "under", "over"]:
-            break
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
 
+    sum = dice1+dice2
 
-dice1 = random.randint(1,6)
-dice2 = random.randint(1,6)
+    print(f"The dice roll is {sum}")
 
-sum = dice1+dice2
+    if sum < 7 and bet == 'under':
+        print("You won")
+        money += (betMoney*2)
 
-print(sum)
+    elif sum == 7 and bet == 'seven':
+        print("You won")
+        money += (betMoney*3)
+    elif sum > 7 and bet == 'over':
+        print("You won")
+        money += (betMoney*2)
+    else:
+        print("You lost.")
+        money -= (betMoney)
 
-if sum < 7 and bet == 'under':
-    print("You won")
-    money + (betMoney*2)
-
-elif sum == 7 and bet == 'seven':
-    print("You won")
-    money + (betMoney*3)
-elif sum > 7 and bet == 'over':
-    print("You won")
-    money + (betMoney*2)
-else:
-    print("You lost.")
-    money + (betMoney*0)
+    print(f"Your current money is: {money}")
