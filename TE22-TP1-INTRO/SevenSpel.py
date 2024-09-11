f326ce2443424dc01bcfd7b4c97052178807dec4
TheHoboDad <axejak056@gmail.com>
2024-09-11 13:01:58 +0200

    commit!

--- TE22-TP1-INTRO/SevenSpel.py
+++ TE22-TP1-INTRO/SevenSpel.py
@@ -3,16 +3,23 @@ import random
 money = 100
 betMoney = 0
 
-while money > 0:
-    while True:
-            while True:
-                 print(f"You have {money} money left.")
-                 betMoney = int(input("How much do you want to bet? "))
-                 if betMoney > 0 and betMoney <= money:
-                      break
-                 else:
-                      print(f"Invalid bet you can bet between 1 and {money}")
-                
+while money > 0: 
+    while True: # Retries loop until a valid bet choice is selected (seven, under or over)
+            while True: # Retries loop until a valid bet amount
+                print(f"You have {money} money left.")
+
+                try:
+                    betMoney = int(input("How much do you want to bet? "))
+                except: # Retry if bet isnt a valid data type (int)
+                    print("Invalid data type")
+                    
+                else: # Check if the bet is a valid amount of money
+                    if betMoney > 0 and betMoney <= money:
+                        money -= betMoney
+                        break
+                    else: # Retry if bet isnt a VALID int
+                        print(f"Invalid bet amount, you can only bet between 1 and {money} money.")
+        
             bet = input("Seven, Under or Over: ").lower()
             if bet in ["seven", "under", "over"]:
                 break
@@ -37,6 +44,5 @@ while money > 0:
         money += (betMoney*2)
     else:
         print("You lost.")
-        money -= (betMoney)
-
-    print(f"Your current money is: {money}")
+        
+print("Out of money")
